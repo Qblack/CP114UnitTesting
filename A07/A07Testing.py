6963e2382d3235247f49059f2c09ab27318a3108
Qblack <blac2410@mylaurier.ca>
2014-03-09 15:27:27 -0400

    Added additional tests

--- A07/A07Testing.py
+++ A07/A07Testing.py
@@ -51,16 +51,75 @@ class TestBST(unittest.TestCase):
         actual = self.m_sut.max_r()
         self.assertEqual(expected, actual, "Not the correct maximum")           
         
-    def testBST_delete_node_NoChildren(self):
+    def testBST_delete_NoChildren(self):
         values = [41, 53, 76, 7, 59, 19, 92, 47, 43, 1, 13]
         for i in values:
             self.m_sut.insert(i)
         to_delete = 1
-        actual = self.m_sut.delete(to_delete)
+        actual = self.m_sut.remove(to_delete)
         self.assertEqual(to_delete, actual, "Did not return correct deleted element")
-           
-        
         return
+    
+    def testBST_delete_EmptyBst(self):
+        values = []
+        for i in values:
+            self.m_sut.insert(i)
+        actual = self.m_sut.remove(1)
+        self.assertIsNone( actual, "Did not return correct deleted element")
+        return
+    
+    def testBST_delete_SingleNode(self):
+        values = [1]
+        for i in values:
+            self.m_sut.insert(i)
+        to_delete = 1
+        actual = self.m_sut.remove(to_delete)
+        self.assertEqual( to_delete,actual, "Did not return correct deleted element")
+        return
+    
+    def testBST_delete_RightChild(self):
+        values = [1,2]
+        for i in values:
+            self.m_sut.insert(i)
+        to_delete = 1
+        actual = self.m_sut.remove(to_delete)
+        self.assertEqual( to_delete,actual, "Did not return correct deleted element")
+        return
+    
+    def testBST_delete_LeftChild(self):
+        values = [2,1]
+        for i in values:
+            self.m_sut.insert(i)
+        to_delete = 2
+        actual = self.m_sut.remove(to_delete)
+        self.assertEqual( to_delete,actual, "Did not return correct deleted element")
+        return
+    
+    def testBST_delete_node_one_child(self):
+        values = [41, 53, 76, 7, 59, 19, 92, 47, 43, 1, 13]
+        for i in values:
+            self.m_sut.insert(i)
+        to_delete = 19
+        actual = self.m_sut.remove(to_delete)
+        self.assertEqual(to_delete, actual, "Did not return correct deleted element")
+
+    def testBST_delete_two_children(self):
+        values = [41, 53, 76, 7, 59, 19, 92, 47, 43, 1, 13]
+        for i in values:
+            self.m_sut.insert(i)
+        to_delete = 53
+        actual = self.m_sut.remove(to_delete)
+        self.assertEqual(to_delete, actual, "Did not return correct deleted element")
+        return
+    
+    def testBST_delete_node_node_not_in_Tree(self):
+        values = [41, 53, 76, 7, 59, 19, 92, 47, 43, 1, 13]
+        for i in values:
+            self.m_sut.insert(i)
+        to_delete = 99
+        actual = self.m_sut.remove(to_delete)
+        self.assertIsNone(actual, "Did not return correct deleted element")
+        
         
 
 if __name__ == "__main__":
